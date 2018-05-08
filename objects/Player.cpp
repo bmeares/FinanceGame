50b1224eb9d9988d758ec775f22f9c9560c98863
Bennett Meares <bennett.meares@gmail.com>
2018-05-08 03:45:34 -0400

    changed initial balance

--- objects/Player.cpp
+++ objects/Player.cpp
@@ -1,6 +1,6 @@
 #include "Player.h"
 
-unsigned long long Player::balance = 10000000;
+unsigned long long Player::balance = 100;
 string Player::choice = "";
 vector<Good> Player::inventory = {};
 vector<Service> Player::services = {};
